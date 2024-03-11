16743ad84ece5c49a9cb7d5838f0b752f9b02da0
yurissva <yuri.4929@aluno.pr.senac.br>
2024-03-11 08:51:42 -0300

    Mais alterações em migrations

--- src/database/knex/migrations/20240311111800_createUsers.js
+++ src/database/knex/migrations/20240311111800_createUsers.js
@@ -1,11 +1,11 @@
 exports.up = (knex) => {
     return knex.schema.createTable("users", (table) => {
       table.increments('id').primary();
-      table.boolean("isAdmin").notNullable();
       table.string("name").notNullable();
       table.string("email").notNullable();
       table.string("phone").notNullable();
       table.string("password").notNullable()
+      table.boolean("isAdmin").defaultTo("false");
      })
   };
   
